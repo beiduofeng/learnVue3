4d11c30e7f3c174b93bd34f74024a08ebb64359b
孙金瑶 <1323969103@qq.com>
2024-06-16 13:15:30 +0800

    todolist

--- src/components/ToDoList/todolist.vue
+++ src/components/ToDoList/todolist.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="a">
+  <div :class="{a:b}">
+  <h5>动态样式绑定</h5>
      <input type="text" v-model="inputvalue"/>
      <button @click="butclick">提交</button>
      <ul>
         <li v-for="m in msg">{{m}}</li>
     
      </ul>
+     <div class="c" v-if="show">123456789</div>
+     <div class="c" v-else>9876543211111111111111111111111111</div>
+     <button @click="showclick">change</button>
   </div>
 </template>
 <script lang='ts'>
@@ -15,8 +19,19 @@ import {reactive,ref} from 'vue'
    }
 </script>
 <script lang='ts' setup>
+   let show=ref('aa')
+   let b=false
    let inputvalue=ref('123456')
    let msg=reactive(['第一课的内容','第二课的内容'])
+
+   function butclick(){
+    msg.push(inputvalue)
+    inputvalue=''
+    b=true
+   }
+   function showclick(){
+    show.value=''
+   }
 </script>
 <style scoped>
 .a{
@@ -27,5 +42,11 @@ import {reactive,ref} from 'vue'
 }
 input {
     margin-right:3px;
+}
+.c{
+    background-color: pink;
+    box-shadow: 0 0 10px;
+    padding: 20px;
+    margin: 15px;
 }
 </style>
