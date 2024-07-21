7d8f0c9eebf0fc5c2d3f903f28926c74ebaf05b2
zzusunjs@qq.com <zzusunjs@qq.com>
2024-07-21 17:58:39 +0800

    feat: 添加shiki代码高亮

--- src/common/codeWrapper/index.vue
+++ src/common/codeWrapper/index.vue
@@ -1,23 +1,56 @@
 <template>
   <div class="code-container">
-    <pre class="code">
-      {{ props.codeContent }}
-    </pre>
+    <div class="tip">code</div>
+    <div v-html="htmlResult"></div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { getCodeHtml  } from '@/utils/index';
+
 const props = defineProps({
   codeContent: String,
-})
+});
+
+const htmlResult = ref('');
+
+async function setHtmlResult () {
+  const code = props.codeContent || '';
+  htmlResult.value = await getCodeHtml(code);
+}
+
+setHtmlResult();
 </script>
 
 <style scoped>
 
 .code-container {
-  padding: 12px;
   margin-top: 12px;
+  padding: 0px 4px;
   border: 1px solid tomato;
+  font-size: 16px;
+  background-color: #fff;
+  position: relative;
+}
+
+.tip {
+  /* 绝对定位 */
+  position: absolute;
+  top: 12px;
+  right: 4px;
+
+  font-size: 12px;
+  height: 18px;
+
+  /* flex 布局 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  min-width: 60px;
 }
 
 </style>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 import markdownIt from 'markdown-it';
+import { createHighlighter } from 'shiki';
 
 let globalMarkDownInstance = null;
 
@@ -13,4 +14,38 @@ export const getMarkDownIt = () => {
   return globalMarkDownInstance;
 }
 
+let globalCodeHighlighter =  null;
 
+/**
+ * 获取代码高亮实例
+ * @returns 
+ */
+export const getGlobalCodeHighlighter = async () => {
+  if (!globalCodeHighlighter) {
+    // `createHighlighter` 是异步的，它会初始化高亮器
+    // 并加载指定的语言和主题。
+    globalCodeHighlighter = await createHighlighter({
+      themes: ['vitesse-light'],
+      langs: ['javascript'],
+    })
+  }
+
+  return globalCodeHighlighter;
+}
+
+/**
+ * 使用 shiki 代码高亮包，将传入的代码字符串转换成有高亮效果的 html 字符串
+ * 注意这里是一个异步函数
+ * reference: https://shiki.tmrs.site/guide/install#%E9%AB%98%E4%BA%AE%E5%99%A8%E7%94%A8%E6%B3%95
+ * @param {string} code 
+ */
+export const getCodeHtml = async (code) => {
+  // 获取高亮实例
+  const instance = await getGlobalCodeHighlighter();
+  // 对 codeToHtml 方法封装，简化传参
+  // 注意这里 instance 有可能是 null, 使用 ? 保证代码不报错
+  return instance?.codeToHtml(code, {
+    lang: 'javascript',
+    theme: 'vitesse-light'
+  }) || '';
+}
