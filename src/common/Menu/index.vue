d84d1aaf1dc9a68e66c764abe708802d3b02dd3a
zzusunjs@qq.com <zzusunjs@qq.com>
2024-08-05 23:53:49 +0800

    feat: 简化菜单

--- src/common/Menu/index.vue
+++ src/common/Menu/index.vue
@@ -1,56 +1,37 @@
 <template>
-  <el-row class="tac">
+  <el-row class="menu-row">
     <el-col :span="24">
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-      >
+      <el-menu default-active="1">
         <el-sub-menu index="1">
           <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
+            <span>基础</span>
           </template>
             <el-menu-item index="1-1">
               <RouterLink to="/helloworld">helloworld</RouterLink>
             </el-menu-item>
-            <el-menu-item index="1-2">
+        </el-sub-menu>
+        <el-sub-menu index="2">
+          <template #title>
+            <span>场景</span>
+          </template>
+            <el-menu-item index="2-1">
               <RouterLink to="/todolist">todoList</RouterLink>
             </el-menu-item>
-            <el-menu-item index="1-3">item three</el-menu-item>
-            <el-menu-item index="1-4">item four</el-menu-item>
         </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
+        
       </el-menu>
     </el-col>
   </el-row>
 </template>
 
 <script lang="ts" setup>
-import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from '@element-plus/icons-vue'
 import { RouterLink } from 'vue-router'
-
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+import { VueRouteConfigs } from '@/router/routeConfig';
 </script>
+
+
+<style scoped>
+.menu-row {
+  min-width: 180px;
+}
+</style>
