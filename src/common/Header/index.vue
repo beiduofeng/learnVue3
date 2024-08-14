dd25c61f9d7055c35f96aeb680da976441ad79ee
孙金瑶 <1323969103@qq.com>
2024-08-14 22:44:27 +0800

    header修改

--- src/common/Header/index.vue
+++ src/common/Header/index.vue
@@ -4,109 +4,46 @@
             <img :src="logoUrl" alt="">
         </div>
         <div class="right">
-            <div class="time">
-                <span> {{ formattedDate }}</span>
-                <span> {{ currentTime }}</span>
-                <!-- 地区选择 -->
-                <span>地区：<input type="text" @keydown.enter="getcity" v-model="searchcity"></span>
-                <span id="weather"> {{ weather }},{{ windDirection }}{{ windPower }}</span>
-            </div>
             <div class="tabs" @click="onTabClick">
-                <span :class="currentclass('js')" id="js">JS</span>
-                <span :class="currentclass('Vue3')" id="Vue3">Vue3</span>
-                <span :class="currentclass('Element')" id="Element">Element +</span>
+                <span :class="currentClass('js')" id="js">JS</span>
+                <span :class="currentClass('Vue3')" id="Vue3">Vue3</span>
+                <span :class="currentClass('Element')" id="Element">Element +</span>
             </div>
             <div class="links">
                 <!-- list map -->
                 <a class="github-box" target="_blank" :href="githubUrl">
-                    <img :src="githuLogoUrl">
+                    <img :src="githubLogoUrl">
                 </a>
             </div>
-
         </div>
     </div>
 </template>
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from 'vue';
-import axios from 'axios';
-import { result } from 'lodash-es';
+import { ref } from 'vue';
+const logoUrl = "/logo.png";
+const githubLogoUrl = '/github.svg';
+const githubUrl = 'https://github.com/beiduofeng/learnVue3';
 
-//搜索地区
-let searchcity = ref('')
-const getcity = (search) => {
-    axios({
-        url: 'http://hmajax.itheima.net/api/weather/city',
-        params: {
-            city: searchcity.value
-        }
-    }).then(result => {
-        console.log(result.data.data[0].code);
-        let searchcityCode = result.data.data[0].code
-        getWeather(searchcityCode)
-        searchcity.value = ''
-    })
-}
-//getcity('上海')
-//时钟
-const currentTime = ref();
-function updateTime() {
-    currentTime.value = new Date().toLocaleTimeString();
-}
-onMounted(() => {
-    const intervalId = setInterval(updateTime, 1000); // 更新时间每秒一次
-    const intervalId1 = setInterval(formattedDate.value, 24 * 60 * 60 * 1000);//一天一gengxin
-    // 组件卸载时清除定时器
-    onUnmounted(() => {
-        clearInterval(intervalId);
-        clearInterval(intervalId1);
-    });
-});
-
-
-// 创建一个计算属性，用于格式化日期
-const formattedDate = computed(() => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0'); // 月份从0开始，所以+1，然后确保是两位数
-    const day = String(today.getDate()).padStart(2, '0'); // 确保日期是两位数
-    return `${year}-${month}-${day}`;
-});
-
-let weather = ''
-let windDirection = ''
-let windPower = ''
-const getWeather = (cityCode) => {
-
-    axios({
-        url: 'http://hmajax.itheima.net/api/weather',
-        params: {
-            city: cityCode
-        }
-    }).then(result => {
-        //console.log(result.data.data);
-        weather = result.data.data.weather
-        windDirection = result.data.data.windDirection
-        windPower = result.data.data.windPower
-
-    })
-}
-getWeather('110100')
-
+// 当前选中的 tab 的 id
 const currentSelected = ref()
+
+/**
+ * 将所触发点击事件的span标签的id名赋值给currentSelected
+ * @param e 
+ */
 const onTabClick = (e) => {
     const id = e.target.id
     currentSelected.value = id
 }
 
-const currentclass = (id) => {
+/**
+ * 判断传入的元素 id 和当前选中的 id 是否相同，相同则返回 'selected', 否则返回空字符
+ * @param id 
+ */
+const currentClass = (id) => {
     return currentSelected.value === id ? 'selected' : ''
 }
-// BOM 知识点
-const logoUrl = "/logo.png";
-const githuLogoUrl = '/github.svg';
-const githubUrl = 'https://github.com/beiduofeng/learnVue3';
 </script>
-
 <style scoped>
 .header {
     height: 50px;
@@ -138,16 +75,6 @@ const githubUrl = 'https://github.com/beiduofeng/learnVue3';
     cursor: pointer;
 }
 
-.time {
-    margin-right: 40px;
-}
-
-.time span {
-    margin-right: 40px;
-    display: inline-block;
-    line-height: 28px;
-
-}
 
 .tabs span:hover {
     color: #358be1;
@@ -163,16 +90,5 @@ const githubUrl = 'https://github.com/beiduofeng/learnVue3';
 
 .links a {
     cursor: pointer;
-}
-
-input {
-    width: 50px;
-    height: 10px;
-}
-
-#weather {
-    padding: 3px;
-    background: linear-gradient(45deg, #e49881, #ed7ca7, #62c5e9, #58d4b7);
-    border-radius: 16%;
 }
 </style>
